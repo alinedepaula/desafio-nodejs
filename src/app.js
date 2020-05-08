da51af02b5084080c0ba862dbb0c4adcbae21ff2
alinedepaula <aline.tramontin@gmail.com>
2020-05-08 10:17:20 -0300

    Projects CRUD. Likes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
-
+const { uuid } = require("uuidv4");
 const app = express();
 
 app.use(express.json());
@@ -11,11 +9,18 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes:0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -27,7 +32,17 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find( repository => repository.id === id);
+
+  if (!repository){
+    return response.status(400).send();
+  }
+  
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
