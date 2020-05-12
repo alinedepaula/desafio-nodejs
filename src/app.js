d32c5aae6d4e7e17feffa1acec971a10aa05ee60
alinedepaula <aline.tramontin@gmail.com>
2020-05-12 18:22:54 -0300

    Desafio finalizado

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ app.get("/repositories", (request, response) => {
 
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
-
   const repository = { id: uuid(), title, url, techs, likes:0 };
 
   repositories.push(repository);
@@ -24,16 +23,45 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;  
+
+  const repositoryIndex = repositories.find(repository => repository.id === id);
+
+  if (!repositoryIndex){
+    return response.status(400).json({ error: 'Repository not found' });
+  } 
+
+  const { title, url, techs} = request.body;
+
+  const repository = { 
+    id, 
+    title, 
+    url,
+    techs,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.find(repository => repository.id === id);
+
+  if (!repositoryIndex){
+    return response.status(400).send();
+  } 
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
-
   const repository = repositories.find( repository => repository.id === id);
 
   if (!repository){
